6086fe1e028c201b520d91f330cf0135d1296cb5
Marzan Islam <marzanislam999@gmail.com>
2022-09-27 23:47:59 +0600

    container

--- day3/my_app/lib/home_page.dart
+++ day3/my_app/lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Color.fromARGB(218, 255, 255, 255),
       appBar: AppBar(
         backgroundColor: Colors.deepOrange,
         centerTitle: true,
@@ -27,21 +28,32 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Container(
+        alignment: Alignment.center,
         margin: EdgeInsets.all(15),
         height: 250,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            gradient: LinearGradient(
-              colors: [
-                Color(0xff98FB98),
-                Color(0xffFFC0CB),
-                Color.fromARGB(255, 220, 233, 43),
-                Color.fromARGB(255, 228, 219, 227),
-                Color.fromARGB(255, 132, 204, 50),
-              ],
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
-            )),
+          boxShadow: [
+            BoxShadow(
+                color: Color.fromARGB(255, 160, 187, 207),
+                offset: Offset(10, 10))
+          ],
+          border: Border.all(color: Colors.black, width: 5),
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            Color(0xff98FB98),
+            Color(0xffFFC0CB),
+            Color.fromARGB(255, 220, 233, 43),
+            Color.fromARGB(255, 228, 219, 227),
+            Color.fromARGB(255, 132, 204, 50),
+          ], begin: Alignment.bottomLeft, end: Alignment.topRight),
+        ),
+        child: Text(
+          'hello, I am Marzan  :)',
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+              color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
--- day3/my_app/lib/main.dart
+++ day3/my_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:my_app/home_page.dart';
 
