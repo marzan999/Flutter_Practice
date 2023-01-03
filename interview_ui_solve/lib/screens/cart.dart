1f823d559ffb0ba19d40c81a7da9949299f0c577
Marzan Islam <marzanislam999@gmail.com>
2023-01-03 22:36:24 +0600

    Update cart.dart

--- interview_ui_solve/lib/screens/cart.dart
+++ interview_ui_solve/lib/screens/cart.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:interview_ui_solve/model/food_info.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -10,8 +13,214 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  final a = FoodInfo.generatedProductList();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 27, 27, 27),
+        body: Padding(
+          padding: const EdgeInsets.all(13.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 212, 168, 165),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Icon(Icons.arrow_back_ios_new),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Order details',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 400,
+                child: ListView.builder(
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        color: Color.fromARGB(255, 105, 105, 105),
+                        child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          height: 60,
+                                          width: 60,
+                                          decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                  image: NetworkImage(
+                                                    a[index].image,
+                                                  ),
+                                                  fit: BoxFit.cover),
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 20),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                a[index].name,
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 20),
+                                              ),
+                                              Text(a[index].description,
+                                                  style: TextStyle(
+                                                    color: Color.fromARGB(
+                                                        255, 192, 190, 190),
+                                                  )),
+                                              Text(
+                                                a[index].price,
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    color: Color.fromARGB(
+                                                        255, 100, 171, 189),
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Container(
+                                        height: 25,
+                                        width: 30,
+                                        decoration: BoxDecoration(
+                                            color: Color.fromARGB(
+                                                255, 103, 158, 153),
+                                            borderRadius:
+                                                BorderRadius.circular(8)),
+                                        child: Icon(
+                                          Icons.remove,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Container(
+                                        height: 25,
+                                        width: 30,
+                                        decoration: BoxDecoration(
+                                            color: Colors.transparent,
+                                            borderRadius:
+                                                BorderRadius.circular(8)),
+                                        child: Center(
+                                            child: Text(
+                                          '1',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 17),
+                                        )),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Container(
+                                        height: 25,
+                                        width: 30,
+                                        decoration: BoxDecoration(
+                                            color: Colors.teal,
+                                            borderRadius:
+                                                BorderRadius.circular(8)),
+                                        child: Icon(
+                                          Icons.add,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )),
+                      );
+                    }),
+              ),
+              Container(
+                height: 190,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 79, 163, 155),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Sub-Total',style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                          Text('4.2\$',style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Delivery Charge',style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                          Text('1.0\$',style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Discount',style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                          Text('0.0\$',style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Total',style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
+                          Text('5.2\$',style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
