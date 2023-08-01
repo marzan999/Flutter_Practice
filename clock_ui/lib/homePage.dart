bc3e29bfd62add562e0b7c22af1d56e5e4ace426
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-08-01 10:14:02 -0700

    Update homePage.dart

--- clock_ui/lib/homePage.dart
+++ clock_ui/lib/homePage.dart
@@ -22,12 +22,14 @@ class _HomePageState extends State<HomePage> {
               Container(
                 height: MediaQuery.of(context).size.height * 0.20.h,
                 decoration: BoxDecoration(
-                    color: Colors.pink,
-                    borderRadius: BorderRadius.circular(25)),
+                    color: Color.fromARGB(255, 240, 58, 118),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(25),
+                        bottomRight: Radius.circular(25))),
               ),
-              Container(
-                child: Container(),
-              ),
+              // Container(
+              //   child: Container(),
+              // ),
               SizedBox(
                 height: 138.h,
               ),
@@ -172,7 +174,8 @@ class _HomePageState extends State<HomePage> {
                                       height: 50.h,
                                       width: 50.w,
                                       decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(50),
+                                          borderRadius:
+                                              BorderRadius.circular(50),
                                           color: Colors.white),
                                       child: Center(
                                           child: Icon(
@@ -187,7 +190,8 @@ class _HomePageState extends State<HomePage> {
                                         height: 20.h,
                                         width: 20.w,
                                         decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(20),
+                                            borderRadius:
+                                                BorderRadius.circular(20),
                                             color: Colors.red),
                                         child: Center(child: Text('21')),
                                       ))
@@ -200,7 +204,7 @@ class _HomePageState extends State<HomePage> {
                         SizedBox(
                           width: 10.w,
                         ),
-                         Column(
+                        Column(
                           children: [
                             Container(
                               height: 60.h,
@@ -213,7 +217,8 @@ class _HomePageState extends State<HomePage> {
                                       height: 50.h,
                                       width: 50.w,
                                       decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(50),
+                                          borderRadius:
+                                              BorderRadius.circular(50),
                                           color: Colors.white),
                                       child: Center(
                                           child: Icon(
@@ -228,8 +233,10 @@ class _HomePageState extends State<HomePage> {
                                         height: 20.h,
                                         width: 20.w,
                                         decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(20),
-                                            color: Color.fromARGB(255, 110, 180, 172)),
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            color: Color.fromARGB(
+                                                255, 110, 180, 172)),
                                         child: Center(child: Text('65')),
                                       ))
                                 ],
@@ -241,7 +248,7 @@ class _HomePageState extends State<HomePage> {
                         SizedBox(
                           width: 10.w,
                         ),
-                         Column(
+                        Column(
                           children: [
                             Container(
                               height: 60.h,
@@ -254,12 +261,14 @@ class _HomePageState extends State<HomePage> {
                                       height: 50.h,
                                       width: 50.w,
                                       decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(50),
+                                          borderRadius:
+                                              BorderRadius.circular(50),
                                           color: Colors.white),
                                       child: Center(
                                           child: Icon(
                                         Icons.house,
-                                        color: const Color.fromARGB(255, 247, 0, 255),
+                                        color: const Color.fromARGB(
+                                            255, 247, 0, 255),
                                       )),
                                     ),
                                   ),
@@ -269,8 +278,10 @@ class _HomePageState extends State<HomePage> {
                                         height: 20.h,
                                         width: 20.w,
                                         decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(20),
-                                            color: const Color.fromARGB(255, 151, 148, 148)),
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            color: const Color.fromARGB(
+                                                255, 151, 148, 148)),
                                         child: Center(child: Text('08')),
                                       ))
                                 ],
@@ -282,31 +293,244 @@ class _HomePageState extends State<HomePage> {
                         SizedBox(
                           width: 10.w,
                         ),
-                        Container(
-                          height: 60.h,
-                          width: 60.w,
-                          color: Colors.red,
-                        ),
-                        SizedBox(
-                          width: 10.w,
+                        Column(
+                          children: [
+                            Container(
+                              height: 60.h,
+                              width: 60.w,
+                              // color: Colors.red,
+                              child: Stack(
+                                children: [
+                                  Center(
+                                    child: Container(
+                                      height: 50.h,
+                                      width: 50.w,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(50),
+                                          color: Colors.white),
+                                      child: Center(
+                                          child: Text(
+                                        '+',
+                                        style: TextStyle(
+                                            fontSize: 45, color: const Color.fromARGB(255, 216, 79, 125)),
+                                      )),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text('  ')
+                          ],
                         ),
                       ],
                     ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 60.h,
+                          width: 60.w,
+                          // color: Colors.red,
+                          child: Center(
+                            child: Container(
+                              height: 50.h,
+                              width: 50.w,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color:
+                                      const Color.fromARGB(218, 253, 252, 252)),
+                              child: Center(child: Icon(Icons.alarm_add)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        Container(
+                          height: 70.h,
+                          width: 70.w,
+                          // color: Colors.red,
+                          child: Center(
+                            child: Container(
+                              height: 120.h,
+                              width: 120.w,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Color.fromARGB(255, 240, 58, 118)),
+                              child: Center(child: Icon(Icons.alarm)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        Container(
+                          height: 60.h,
+                          width: 60.w,
+                          // color: Colors.red,
+                          child: Center(
+                            child: Container(
+                              height: 50.h,
+                              width: 50.w,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Color.fromARGB(218, 253, 252, 252)),
+                              child: Center(child: Icon(Icons.alarm_off)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               )
             ],
           ),
           Positioned(
-            top: MediaQuery.of(context).size.height * 0.1.h,
+            top: MediaQuery.of(context).size.height * 0.15,
             left: MediaQuery.of(context).size.width * 0.15.h,
+            right: MediaQuery.of(context).size.width * 0.15.h,
             child: Container(
               child: CircleAvatar(
-                radius: 120.sp,
+                radius: 120,
                 backgroundImage: AssetImage('images/5.jpg'),
               ),
             ),
           ),
+          Positioned(
+             top: MediaQuery.of(context).size.height * 0.1,
+             left: MediaQuery.of(context).size.width * 0.1.h,
+            // right: MediaQuery.of(context).size.width * 0.15.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 10,
+                backgroundColor: Color.fromARGB(255, 192, 168, 176),
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height * 0.15,
+            // left: MediaQuery.of(context).size.width * 0.1.h,
+             right: MediaQuery.of(context).size.width * 0.15.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 12,
+                backgroundColor: Color.fromARGB(255, 192, 168, 176),
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height * 0.1,
+            // left: MediaQuery.of(context).size.width * 0.1.h,
+             right: MediaQuery.of(context).size.width * 0.25.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 4,
+                backgroundColor: Color.fromARGB(255, 192, 168, 176),
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.3,
+          // left: MediaQuery.of(context).size.width * 0.1.h,
+            right: MediaQuery.of(context).size.width * 0.1.h,
+           // bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 10,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.5,
+          // left: MediaQuery.of(context).size.width * 0.1.h,
+            right: MediaQuery.of(context).size.width * 0.1.h,
+           // bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.5,
+          left: MediaQuery.of(context).size.width * 0.1.h,
+           // right: MediaQuery.of(context).size.width * 0.1.h,
+           // bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 3,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.4,
+          // left: MediaQuery.of(context).size.width * 0.1.h,
+            right: MediaQuery.of(context).size.width * 0.15.h,
+           // bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 4,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.2,
+           left: MediaQuery.of(context).size.width * 0.1.h,
+           // right: MediaQuery.of(context).size.width * 0.1.h,
+            bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 6,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.3,
+           left: MediaQuery.of(context).size.width * 0.1.h,
+           // right: MediaQuery.of(context).size.width * 0.1.h,
+            bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 4,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.4,
+           left: MediaQuery.of(context).size.width * 0.15.h,
+           // right: MediaQuery.of(context).size.width * 0.1.h,
+            bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 10,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          Positioned(
+             top: MediaQuery.of(context).size.height*.5,
+          // left: MediaQuery.of(context).size.width * 0.2.h,
+            right: MediaQuery.of(context).size.width * 0.1.h,
+            bottom: MediaQuery.of(context).size.width * 1.h,
+            child: Container(
+              child: CircleAvatar(
+                radius: 4,
+                backgroundColor: Colors.pink,
+              ),
+            ),
+          ),
+          
         ],
       ),
     );
@@ -314,35 +538,3 @@ class _HomePageState extends State<HomePage> {
 }
 
 
-
-
-
-//  Stack(
-//                           children: [
-//                             Container(
-//                               height: 50.h,
-//                               width: 50.w,
-//                               decoration: BoxDecoration(
-//                                   borderRadius: BorderRadius.circular(5),
-//                                   color: Colors.white),
-//                               child: Center(
-//                                   child: Icon(
-//                                 Icons.cake,
-//                                 color: Colors.orange,
-//                               )),
-//                             )
-//                           ],
-//                         ),
-//                         Positioned(
-//                             child: Container(
-//                           height: 20.h,
-//                           width: 20.w,
-//                           decoration: BoxDecoration(
-//                               borderRadius: BorderRadius.circular(5),
-//                               color: Colors.white),
-//                           child: Center(
-//                               child: Icon(
-//                             Icons.cake,
-//                             color: Colors.orange,
-//                           )),
-//                         ))
