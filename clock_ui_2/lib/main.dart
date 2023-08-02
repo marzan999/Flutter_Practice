240003e35babe90b0fb896a7b817914b1995cb36
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-08-02 02:18:19 -0700

    Update main.dart

--- clock_ui_2/lib/main.dart
+++ clock_ui_2/lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
         builder: (context, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
